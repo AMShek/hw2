b69499314f1cb64d5890cadf24eacc7600aadb71
Amber Shek <mibybymi@163.com>
2024-09-10 04:51:45 +0000

    add test case #3

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ def test_case_3():
     arr_in = [5, 5, 1, 6, -1, 4, 3, 7, 5]
     arr_sorted = [-1, 1, 3, 4, 5, 5, 5, 6, 7]
     arr_out = merge_sort(arr_in)
-    assert arr_out == arr_sorted 
+    assert arr_out == arr_sorted
